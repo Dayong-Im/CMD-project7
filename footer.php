4fe365563ca7b15c6da474586f669b5a274be8a6
JanBe <ppk1998@naver.com>
2022-05-25 22:16:33 +0900

    Ws main commit

---
+++ footer.php
@@ -0,0 +1,12 @@
+        <div id="footer_content">
+            <p id="footer_logo"> Europe | <span>Sport</span></p>
+            <ul id="download">
+                <li>other site</li>
+                <li>- 네이버 스포츠<br>
+                    (https://sports.news.naver.com)</li>
+            </ul>
+            <ul id="author">
+                <li>문의 메일</li>
+                <li>- 메일 주소 : ppk1998@naver.com</li>
+            </ul>
+        </div>
---
+++ header.php
@@ -0,0 +1,42 @@
+﻿<?php
+    session_start();
+    if (isset($_SESSION["userid"])) $userid = $_SESSION["userid"];
+    else $userid = "";
+    if (isset($_SESSION["username"])) $username = $_SESSION["username"];
+    else $username = "";
+?>		
+        <div id="top">
+            <h3>
+                <a href="index.php" >U N L</a>
+            </h3>
+            <ul id="top_menu">  
+<?php
+    if(!$userid) {
+?>                
+                <li><a href="member_form.php">회원 가입</a> </li>
+                <li> | </li>
+                <li><a href="login_form.php">로그인</a></li>
+<?php
+    } else {
+                $logged = $username."(".$userid.")님";
+?>
+                <li><?=$logged?> </li>
+                <li> | </li>
+                <li><a href="logout.php">로그아웃</a> </li>
+                <li> | </li>
+                <li><a href="member_modify_form.php">정보 수정</a></li>
+<?php
+    }
+?>
+            </ul>
+        </div>
+        <div id="menu_bar">
+            <ul>  
+                <li><a href="index.php">프리미어리그</a></li>
+                <li><a href="message_form.php">라리가</a></li>                                
+                <li><a href="board_form.php">분데스리가</a></li>
+                <li><a href="index.php">세리에A</a></li>
+                <li><a href="#">리그앙1</a></li>
+                <li><a href="#">게시판</a></li>
+            </ul>
+        </div>
